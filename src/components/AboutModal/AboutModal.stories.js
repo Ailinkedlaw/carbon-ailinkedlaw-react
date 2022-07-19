cb3069621c51feb93a2ab398bc44ed422c936594
uyoung <i@uyoung.co>
2022-07-19 14:24:16 +0800

    新增组件AboutModal、APIKeyModal、CreateTearsheetNarrow和RemoveModal

---
+++ src/components/AboutModal/AboutModal.stories.js
@@ -0,0 +1,249 @@
+/**
+ * Copyright IBM Corp. 2020, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// cspell:words grafana
+
+import React, { useEffect, useState } from 'react'
+
+import { pkg } from '@/settings'
+import { prepareStory, } from '@/global/js/utils/story-helper'
+
+import { AboutModal } from '.'
+
+import { Button, Link } from '@carbon/react'
+
+import mdx from './AboutModal.mdx'
+
+import ExampleLogo from './_story-assets/example-logo.svg'
+import ansibleLogo from './_story-assets/ansible-logo.png'
+import grafanaLogo from './_story-assets/grafana-logo.png'
+import jsLogo from './_story-assets/js-logo.png'
+
+import styles from './_storybook-styles.scss'
+
+const blockClass = `${pkg.prefix}--about-modal`
+
+export default {
+  title: 'Components/AboutModal',
+  component: AboutModal,
+  parameters: {
+    styles,
+    docs: { page: mdx },
+    controls: { sort: 'requiredFirst' },
+  },
+  argTypes: {
+    additionalInfo: {
+      control: {
+        type: 'select',
+        labels: {
+          0: 'none',
+          1: 'version number',
+          2: 'version number and technologies used',
+        },
+      },
+      options: [0, 1, 2],
+      mapping: {
+        0: null,
+        1: [{ label: 'Version number', content: '1.3.41' }],
+        2: [
+          { label: 'Version number', content: '1.3.41' },
+          {
+            label: 'Technologies used',
+            content: (
+              <>
+                <img
+                  src={grafanaLogo}
+                  alt="Grafana"
+                  className="about-modal-stories--tech-logo"
+                />
+                <img
+                  src={ansibleLogo}
+                  alt="Ansible"
+                  className="about-modal-stories--tech-logo"
+                />
+                <img
+                  src={jsLogo}
+                  alt="JavaScript"
+                  className="about-modal-stories--tech-logo"
+                />
+              </>
+            ),
+          },
+        ],
+      },
+    },
+    links: {
+      control: {
+        type: 'select',
+        labels: { 0: 'none', 1: 'one link', 2: 'two links' },
+      },
+      options: [0, 1, 2],
+      mapping: {
+        0: null,
+        1: [
+          <Link href="https://www.carbondesignsystem.com" key="link1">
+            Carbon Design System
+          </Link>,
+        ],
+        2: [
+          <Link href="https://www.carbondesignsystem.com" key="link1">
+            Carbon Design System
+          </Link>,
+          <Link href="https://www.ibm.com/design/language" key="link2">
+            IBM Design Language
+          </Link>,
+        ],
+      },
+    },
+    logo: {
+      control: {
+        disable: true,
+      },
+    },
+    open: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+}
+
+const logo = (
+  <img
+    src={ExampleLogo}
+    alt="Example product or service logo"
+    style={{ maxWidth: '6rem' }}
+  />
+)
+
+const Template = (storyName, storyInitiallyOpen, props) => {
+  const [open, setOpen] = useState(storyInitiallyOpen)
+  const [beenOpen, setBeenOpen] = useState(false)
+  useEffect(() => setBeenOpen(beenOpen || open), [open, beenOpen])
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>
+        {beenOpen ? 'Reopen the' : 'Open the'} {storyName}
+      </Button>
+
+      <style>{`.${blockClass} { opacity: 0; }`};</style>
+      <AboutModal
+        {...props}
+        onClose={() => setOpen(false)}
+        logo={logo}
+        open={open}
+        tabListAriaLabel="Product info"
+      />
+    </>
+  )
+}
+
+Template.propTypes = AboutModal.propTypes
+
+const commonArgs = {
+  closeIconDescription: 'Close',
+  title: (
+    <>
+      IBM{' '}
+      <span
+        style={
+          // stylelint-disable-next-line carbon/type-token-use
+          { fontWeight: '600' }
+        }
+      >
+        Watson AI Ops
+      </span>
+    </>
+  ),
+  content: <>This is example content for an About Modal.</>,
+}
+
+const aboutModalStoryName = 'About modal'
+export const aboutModal = prepareStory(
+  Template.bind({}, aboutModalStoryName, true),
+  {
+    storyName: aboutModalStoryName,
+    args: {
+      additionalInfo: 1,
+      ...commonArgs,
+    },
+  }
+)
+
+const withAllBodyStoryName =
+  'About modal with links and legal and copyright text'
+export const withAllBody = prepareStory(
+  Template.bind({}, withAllBodyStoryName, true),
+  {
+    storyName: withAllBodyStoryName,
+    args: {
+      additionalInfo: 1,
+      copyrightText: <>Copyright &copy; 2020 IBM corporation</>,
+      legalText:
+        'This Web site contains proprietary notices and copyright information, ' +
+        'the terms of which must be observed and followed.' +
+        ' Please see the tab entitled “Copyright and trademark information” for related information.',
+      links: 2,
+      ...commonArgs,
+    },
+  }
+)
+
+const withAddInfoStoryName = 'About modal with additional info'
+export const withAddInfo = prepareStory(
+  Template.bind({}, withAddInfoStoryName, true),
+  {
+    storyName: withAddInfoStoryName,
+    args: {
+      additionalInfo: 2,
+      ...commonArgs,
+    },
+  }
+)
+
+const fullyLoadedStoryName = 'About modal with all props set'
+export const fullyLoaded = prepareStory(
+  Template.bind({}, fullyLoadedStoryName, false),
+  {
+    storyName: fullyLoadedStoryName,
+    args: { ...withAllBody.args, ...withAddInfo.args },
+  }
+)
+
+const withDarkThemeStoryName = 'About modal using dark theme'
+export const withDarkTheme = prepareStory(
+  Template.bind({}, withDarkThemeStoryName, false),
+  {
+    storyName: withDarkThemeStoryName,
+    args: {
+      additionalInfo: 1,
+      className: 'sb--use-carbon-theme-g90',
+      ...commonArgs,
+    },
+  }
+)
+
+const withScrollStoryName = 'About modal with scrolling'
+export const withScroll = prepareStory(
+  Template.bind({}, withScrollStoryName, true),
+  {
+    storyName: withScrollStoryName,
+    args: {
+      additionalInfo: 1,
+      copyrightText: <>Copyright &copy; 2020 IBM corporation</>,
+      legalText:
+        'This Web site contains proprietary notices and copyright information, ' +
+        'the terms of which must be observed and followed. Please see the tab entitled “Copyright and trademark information” for related information. ' +
+        'IBM grants you a non-exclusive, non-transferable, ' +
+        'limited permission to access and display the Web pages within this site as a customer or potential customer of IBM provided you comply with these Terms of Use, ' +
+        'and all copyright, trademark, and other proprietary notices remain intact. You may only use a crawler to crawl this Web site as permitted by this Web site’s robots.txt protocol, ' +
+        'and IBM may block any crawlers in its sole discretion. The use authorized under this agreement is non-commercial in nature (e.g., you may not sell the content you access on or through this Web site.) All other use of this site is prohibited. Except for the limited permission in the preceding paragraph, IBM does not grant you any express or implied rights or licenses under any patents, trademarks, copyrights, or other proprietary or intellectual property rights. You may not mirror any of the content from this site on another Web site or in any other media. Any software and other materials that are made available for downloading, access, or other use from this site with their own license terms will be governed by such terms, conditions, and notices. Your failure to comply with such terms or any of the terms on this site will result in automatic termination of any rights granted to you, without prior notice, and you must immediately destroy all copies of downloaded materials in your possession, custody or control. This Web site contains proprietary notices and copyright information, the terms of which must be observed and followed. Please see the tab entitled “Copyright and trademark information” for related information. IBM grants you a non-exclusive, non-transferable, limited permission to access and display the Web pages within this site as a customer or potential customer of IBM provided you comply with these Terms of Use, and all copyright, trademark, and other proprietary notices remain intact. You may only use a crawler to crawl this Web site as permitted by this Web site’s robots.txt protocol, and IBM may block any crawlers in its sole discretion. The use authorized under this agreement is non-commercial in nature (e.g., you may not sell the content you access on or through this Web site.) All other use of this site is prohibited. Except for the limited permission in the preceding paragraph, IBM does not grant you any express or implied rights or licenses under any patents, trademarks, copyrights, or other proprietary or intellectual property rights. You may not mirror any of the content from this site on another Web site or in any other media. Any software and other materials that are made available for downloading, access, or other use from this site with their own license terms will be governed by such terms, conditions, and notices. Your failure to comply with such terms or any of the terms on this site will result in automatic termination of any rights granted to you, without prior notice, and you must immediately destroy all copies of downloaded materials in your possession, custody or control.',
+      ...commonArgs,
+    },
+  }
+)
---
+++ src/components/AboutModal/AboutModal.test.js
@@ -0,0 +1,221 @@
+/**
+ * Copyright IBM Corp. 2021, 2021
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// cspell:words grafana
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { pkg } from '../../settings';
+
+import uuidv4 from '../../global/js/utils/uuidv4';
+
+import { Link } from '@carbon/react';
+import { AboutModal } from '.';
+
+import ExampleLogo from './_story-assets/example-logo.svg';
+import ansibleLogo from './_story-assets/ansible-logo.png';
+import grafanaLogo from './_story-assets/grafana-logo.png';
+import jsLogo from './_story-assets/js-logo.png';
+
+const carbonPrefix = 'cds';
+const blockClass = `${pkg.prefix}--about-modal`;
+const componentName = AboutModal.displayName;
+
+const tabLabel1 = `Version number ${uuidv4()}`;
+const tabLabel2 = `Technologies (${uuidv4()}) used`;
+const additionalInfo = [
+  { label: tabLabel1, content: '1.3.41' },
+  {
+    label: tabLabel2,
+    content: (
+      <>
+        <img
+          src={grafanaLogo}
+          alt="Grafana"
+          className="about-modal-stories--tech-logo"
+        />
+        <img
+          src={ansibleLogo}
+          alt="Ansible"
+          className="about-modal-stories--tech-logo"
+        />
+        <img
+          src={jsLogo}
+          alt="JavaScript"
+          className="about-modal-stories--tech-logo"
+        />
+      </>
+    ),
+  },
+];
+const className = `class-${uuidv4()}`;
+const closeIconDescription = `close ${uuidv4()}`;
+const content = `This is example content: ${uuidv4()}`;
+const copyrightText = `Copyright test text ${uuidv4()}`;
+const dataTestId = uuidv4();
+const logoAltText = `Example product ${uuidv4()} logo`;
+const logo = (
+  <img src={ExampleLogo} alt={logoAltText} style={{ maxWidth: '6rem' }} />
+);
+const legalText = `Legal test text ${uuidv4()}`;
+const linkText = `Carbon (${uuidv4()}) Design System`;
+const linkHref = `https://www.carbondesignsystem.com/${uuidv4()}`;
+const links = [
+  <Link href={linkHref} key="link1">
+    {linkText}
+  </Link>,
+  <Link href="https://www.ibm.com/design/language" key="link2">
+    IBM Design Language
+  </Link>,
+];
+const onCloseReturnsTrue = jest.fn(() => true);
+const onCloseReturnsFalse = jest.fn(() => false);
+const titleText = `Watson ${uuidv4()} Ops`;
+const title = (
+  <>
+    IBM <span>{titleText}</span>
+  </>
+);
+const versionNumber = `1.3.${uuidv4()}`;
+
+// render an AboutModal with content, logo, title, and any other required props
+const renderComponent = ({ ...rest }) =>
+  render(
+    <AboutModal
+      {...{ closeIconDescription, content, logo, title, ...rest }}
+      tabListAriaLabel="Product info"
+    />
+  );
+
+describe(componentName, () => {
+  const { ResizeObserver } = window;
+
+  beforeEach(() => {
+    window.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn(),
+    }));
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    window.ResizeObserver = ResizeObserver;
+  });
+
+  it('renders a component AboutModal', () => {
+    renderComponent({ open: true });
+    expect(screen.getByRole('presentation')).toHaveClass(blockClass);
+  });
+
+  it.skip('has no accessibility violations', async () => {
+    const { container } = renderComponent();
+    await expect(container).toBeAccessible(componentName);
+    await expect(container).toHaveNoAxeViolations();
+  });
+
+  it('renders closeIconDescription, title, logo, and content', () => {
+    renderComponent({ open: true });
+    screen.getByRole('button', { name: closeIconDescription });
+    screen.getByText(titleText);
+    screen.getByText(content);
+    screen.getByAltText(logoAltText);
+  });
+
+  it('renders with links', () => {
+    renderComponent({ links, open: true });
+    const link = screen.getByRole('link', { name: linkText });
+    expect(link.href).toEqual(linkHref);
+  });
+
+  it('renders legal text', () => {
+    renderComponent({ legalText, open: true });
+    screen.getByText(legalText);
+  });
+
+  it('renders copyright text', () => {
+    renderComponent({ copyrightText, open: true });
+    screen.getByText(copyrightText);
+  });
+
+  it('renders a clickable carbon tab for additional info', () => {
+    renderComponent({ additionalInfo, open: true });
+    const tabToSelect = screen.getByRole('tab', { name: tabLabel2 });
+    const tabSelected = `${carbonPrefix}--tabs__nav-item--selected`;
+    expect(tabToSelect).not.toHaveClass(tabSelected);
+    userEvent.click(tabToSelect);
+    expect(tabToSelect).toHaveClass(tabSelected);
+  });
+
+  it('renders a version number', () => {
+    renderComponent({
+      additionalInfo: [{ label: tabLabel1, content: versionNumber }],
+    });
+    screen.getByText(versionNumber);
+  });
+
+  it('is visible when open is true', () => {
+    renderComponent({ open: true });
+    expect(screen.getByRole('presentation')).toHaveClass('is-visible');
+  });
+
+  it('is not visible when open is not true', () => {
+    const { container } = renderComponent({ open: false });
+    expect(container.firstChild).not.toHaveClass('is-visible');
+  });
+
+  it('applies className to the root node', () => {
+    renderComponent({ className, open: true });
+    expect(screen.getByRole('presentation')).toHaveClass(className);
+  });
+
+  it('calls onClose() when modal is closed', () => {
+    renderComponent({ open: true, onClose: onCloseReturnsTrue });
+    const aboutModal = screen.getByRole('presentation');
+    const closeButton = screen.getByRole('button', {
+      name: closeIconDescription,
+    });
+    expect(aboutModal).toHaveClass('is-visible');
+    expect(onCloseReturnsTrue).toHaveBeenCalledTimes(0);
+    userEvent.click(closeButton);
+    expect(onCloseReturnsTrue).toHaveBeenCalledTimes(1);
+  });
+
+  it('allows veto when modal is closed', () => {
+    renderComponent({ open: true, onClose: onCloseReturnsFalse });
+    const aboutModal = screen.getByRole('presentation');
+    const closeButton = screen.getByRole('button', {
+      name: closeIconDescription,
+    });
+    expect(aboutModal).toHaveClass('is-visible');
+    expect(onCloseReturnsFalse).toHaveBeenCalledTimes(0);
+    userEvent.click(closeButton);
+    expect(aboutModal).toHaveClass('is-visible');
+    expect(onCloseReturnsFalse).toHaveBeenCalledTimes(1);
+  });
+
+  it('adds additional properties to the containing node', () => {
+    renderComponent({ 'data-testid': dataTestId });
+    screen.getByTestId(dataTestId);
+  });
+
+  it('forwards a ref to an appropriate node', () => {
+    const ref = React.createRef();
+    renderComponent({ ref });
+    expect(ref.current.outerModal.current).toHaveClass(blockClass);
+  });
+
+  it('adds the Devtools attribute to the containing node', () => {
+    renderComponent({ 'data-testid': dataTestId });
+
+    expect(screen.getByTestId(dataTestId)).toHaveDevtoolsAttribute(
+      componentName
+    );
+  });
+});
