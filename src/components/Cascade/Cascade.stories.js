6722b99f1d95d2ca0d6049fd51baf92fd4c63f24
uyoung <i@uyoung.co>
2022-07-19 12:43:47 +0800

    新增组件Cascade

---
+++ src/components/Cascade/Cascade.stories.js
@@ -0,0 +1,72 @@
+//
+// Copyright IBM Corp. 2021, 2021
+//
+// This source code is licensed under the Apache-2.0 license found in the
+// LICENSE file in the root directory of this source tree.
+//
+
+import React from 'react'
+import {
+  prepareStory,
+} from '@/global/js/utils/story-helper'
+import { Cascade } from '.'
+import mdx from './Cascade.mdx'
+import styles from './_storybook-styles.scss' // import index in case more files are added later.
+import { Column } from '@carbon/react'
+
+export default {
+  title: 'Components/Cascade',
+  component: Cascade,
+  parameters: {
+    styles,
+    docs: {
+      page: mdx,
+    },
+  },
+}
+
+const DefaultTemplate = (args) => {
+  return (
+    <Cascade {...args}>
+      <div className="box" />
+      <div className="box" />
+      <div className="box" />
+      <div className="box" />
+      <div className="box" />
+      <div className="box" />
+      <div className="box" />
+      <div className="box" />
+    </Cascade>
+  )
+}
+
+const GridTemplate = (args) => {
+  const getBoxes = () => {
+    const boxes = []
+    for (let i = 0; i < 4; i ++) {
+      boxes.push(
+        <Column lg={4}>
+          <div className="grid-box" />
+        </Column>
+      )
+    }
+    return boxes
+  }
+
+  return (
+    <Cascade {...args}>
+      {getBoxes()}
+      {getBoxes()}
+    </Cascade>
+  )
+}
+
+export const WithoutGrid = prepareStory(DefaultTemplate, {
+  args: {},
+})
+
+export const WithGrid = prepareStory(GridTemplate, {
+  args: {
+    grid: true,
+  },
+})
